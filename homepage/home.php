c69235e7f79e72845a191b2a13474d4b0193a2a1
vreshly <vreshly13@gmail.com>
2023-12-02 11:36:08 +0700

    HOMEPAGE
    homepage update

---
+++ homepage/home.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <title>home</title>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400%2C500%2C600"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Axiforma%3A400%2C600"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Plus+Jakarta+Sans%3A400%2C500"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Poppins%3A400%2C500%2C600"
+    />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="home">
+      <div class="group">
+        <div class="frame-01">
+          <div class="frame-02">
+            <p class="minimal">
+              <span class="minimal-sub1">Mini</span>
+              <span class="minimal-sub2">mal</span>
+            </p>
+            <div class="frame-home">
+              <p href="#" class="homme">Home</p>
+              <a href="folder/card.html" class="shop">Shop</a>
+            </div>
+          </div>
+          <div class="frame-logo-01">
+            <img class="keranjang" src="logo keranjang.png" />
+            <img class="profile" src="logo profile.png" />
+          </div>
+        </div>
+        <p class="discover">Quality Craftsmanship Timeless Appeal</p>
+        <div class="auto-group">
+          <div class="auto-group-1">
+            <div class="frame-explore">Explore All Products</div>
+            <div class="auto-group-REQ"></div>
+          </div>
+        </div>
+      </div>
+      <div class="frame-unique">
+        <div class="frame-unique-1">
+          <div class="frame-unique-2">
+            <p class="item-150">150+</p>
+            <p class="unique-style">Unique Style</p>
+          </div>
+          <div class="frame-delivery">
+            <p class="item-310">310+</p>
+            <p class="delivery-product">Delivery Product</p>
+          </div>
+          <div class="frame-happy">
+            <p class="item-295">295+</p>
+            <p class="happy-customer">Happy Customer</p>
+          </div>
+        </div>
+        <div class="frame-unique-penutup"></div>
+      </div>
+      <div class="frame-tengah">
+        <div class="auto-group-tengah">
+          <div class="auto-group-tengah-1">
+            <img class="gambar-tengah" src="gambar tengah.png" />
+            <div class="rectangle"></div>
+            <p class="minimal">
+              <span class="minimal-sub-1">Mini</span>
+              <span class="minimal-sub-2">mal</span>
+            </p>
+          </div>
+          <div class="auto-group-our">
+            <p class="our-passion">
+              Our Passion For Design,
+              <br />
+              Attention To Detail
+            </p>
+            <p class="our-mission">
+              Our mission is to turn your vision into reality.
+              <br />
+              We collaborate closely with our clients to understand
+              <br />
+              their desires and requirements, ensuring that every project
+              <br />
+              we undertake is a true reflection of their style
+              <br />
+              and preferences.
+            </p>
+            <div class="explore-all">Explore All Products</div>
+          </div>
+        </div>
+        <div class="frame-tutup"></div>
+      </div>
+      <div class="frame-bawah">
+        <div class="group-bawah">
+          <p class="our-best">Our Best Seller Product</p>
+          <div class="frame-bawah-putih">
+            <div class="frame-gambar-putih">
+              <img class="rak-putih" src="rak putih.png" />
+              <p class="brimnes">BRIMNES</p>
+              <p class="cabinet">Cabinet with doors, white, 78x95 cm</p>
+              <p class="harga">Rp 1.299.000</p>
+            </div>
+            <div class="frame-gambar-minimalis">
+              <div class="frame-16">
+                <img class="kursi-minimalis" src="kursi minimalis.png" />
+                <p class="minimalist">Minimalist Chair</p>
+                <p class="cabinet">Cabinet with doors, white, 78x95 cm</p>
+                <p class="harga">Rp 1.400.000</p>
+              </div>
+              <div class="frame-17">
+                <img class="meja-rak" src="meja rak kayu.png" />
+                <p class="table-shelf">Table Shelf Wood</p>
+                <p class="cabinet-kayu">Cabinet with doors, white, 78x95 cm</p>
+                <p class="harga">Rp 1.110.000</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="discount">
+          <p class="get-discount">Get Discount <br />up to 50% off Today!</p>
+          <div class="explore">Explore All Products</div>
+        </div>
+      </div>
+      <div class="frame-alamat">
+        <div class="group-alamat">
+          <p class="jalan">
+            Jl. Kedoyan, kec, batam kota,
+            <br />
+            Kota Batam, Kepulauan Riau
+          </p>
+          <div class="logo">
+            <img class="fb" src="logo fb.png" />
+            <img class="ig" src="logo ig.png" />
+          </div>
+          <div class="bagian-minimal">
+            <p class="minimal-01">Minimal</p>
+            <p class="minimal-02">Minimal</p>
+          </div>
+        </div>
+        <div class="group-minimal">
+          <p class="sub-minimal">
+            <span class="minimal-sub-1">Mini</span>
+            <span class="minimal-sub-2">mal</span>
+          </p>
+          <div class="home-about-shop">
+            <p class="homme">Home</p>
+            <p class="shop">Shop</p>
+          </div>
+        </div>
+        <div class="privacy"></div>
+        <div class="group-privacy">
+          <p class="privacy-polisi">Privacy &amp; Policy</p>
+          <p class="copyright">Copyright © minimal 2023</p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
